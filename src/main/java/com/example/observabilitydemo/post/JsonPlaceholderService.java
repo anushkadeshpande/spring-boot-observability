517dc0f13028789fb3286b6c9a7a8871854d5ea3
anushkadeshpande <anushkaa5000@gmail.com>
2023-10-15 00:30:13 +0530

    feat: Added JsonPlaceHolderService interface

---
+++ src/main/java/com/example/observabilitydemo/post/JsonPlaceholderService.java
@@ -0,0 +1,15 @@
+package com.example.observabilitydemo.post;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.service.annotation.GetExchange;
+
+public interface JsonPlaceholderService {
+    
+    @GetExchange("/posts")
+    List<Post> findAll();
+
+    @GetExchange("/posts/{id}")
+    Post findById(@PathVariable Integer id);
+}
