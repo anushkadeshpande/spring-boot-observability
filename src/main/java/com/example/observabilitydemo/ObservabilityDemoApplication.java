4e4da8d8ed9d80d56f72f95de7c18865bf724900
anushkadeshpande <anushkaa5000@gmail.com>
2023-10-15 01:15:44 +0530

    feat: Added CommandLineRunner for bootstrapping posts

--- src/main/java/com/example/observabilitydemo/ObservabilityDemoApplication.java
+++ src/main/java/com/example/observabilitydemo/ObservabilityDemoApplication.java
@@ -1,5 +1,10 @@
 package com.example.observabilitydemo;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -8,9 +13,12 @@ import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import com.example.observabilitydemo.post.JsonPlaceholderService;
+import com.example.observabilitydemo.post.Post;
 
 @SpringBootApplication
 public class ObservabilityDemoApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(ObservabilityDemoApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ObservabilityDemoApplication.class, args);
@@ -21,5 +29,13 @@ public class ObservabilityDemoApplication {
 		RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com");
 		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
 		return factory.createClient(JsonPlaceholderService.class);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(JsonPlaceholderService jsonPlaceholderService) {
+		return args -> {
+			List<Post> posts = jsonPlaceholderService.findAll();
+			log.info("Posts: {}", posts.size());
+		};
 	}
 }
