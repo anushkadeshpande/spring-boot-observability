1bb28e42438ac1e08a04a9e60509e2179d05dc6e
anushkadeshpande <anushkaa5000@gmail.com>
2023-10-15 00:38:37 +0530

    feat: Added a RestClient for JsonPlaceholderService

--- src/main/java/com/example/observabilitydemo/ObservabilityDemoApplication.java
+++ src/main/java/com/example/observabilitydemo/ObservabilityDemoApplication.java
@@ -2,6 +2,12 @@ package com.example.observabilitydemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import com.example.observabilitydemo.post.JsonPlaceholderService;
 
 @SpringBootApplication
 public class ObservabilityDemoApplication {
@@ -10,4 +16,10 @@ public class ObservabilityDemoApplication {
 		SpringApplication.run(ObservabilityDemoApplication.class, args);
 	}
 
+	@Bean
+	JsonPlaceholderService jsonPlaceholderService() {
+		RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com");
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+		return factory.createClient(JsonPlaceholderService.class);
+	}
 }
